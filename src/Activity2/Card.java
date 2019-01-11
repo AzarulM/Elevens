d92300bc3744b53e6b149671f043833bfaba626a
AzarulM <43213109+AzarulM@users.noreply.github.com>
2019-01-11 11:39:18 -0500

    Update Card.java

--- src/Activity2/Card.java
+++ src/Activity2/Card.java
@@ -4,4 +4,42 @@ package Activity2;
  * Created by Teacher on 1/7/2019.
  */
 public class Card {
+
+    String rank;
+    String suit;
+    int pointValue;
+
+    public Card(String rank, String suit, int pointValue)
+    {
+        this.rank = rank;
+        this.suit = suit;
+        this.pointValue = pointValue;
+    }
+
+
+    public String getRank()
+    {
+        return rank;
+    }
+
+    public String getSuit()
+    {
+        return suit;
+    }
+
+    public int getPointValue()
+    {
+        return pointValue;
+    }
+
+    public boolean equals(Card othercard)
+    {
+        return this.pointValue == othercard.pointValue;
+    }
+
+    public String toString()
+    {
+        return this.rank + " of " + this.suit;
+    }
+
 }
