e5e777eb01182f7113f26aad06e223acc0d0523f
AzarulM <43213109+AzarulM@users.noreply.github.com>
2019-01-11 11:37:11 -0500

    Update CardTester.java

--- src/Activity1/CardTester.java
+++ src/Activity1/CardTester.java
@@ -4,4 +4,9 @@ package Activity1;
  * Created by Teacher on 1/7/2019.
  */
 public class CardTester {
+
+    Card card1 = new Card("1", "A", 100);
+    Card card2 = new Card("2", "B", 200);
+    Card card3 = new Card("3", "C", 300);
+    Card[] cards = {card1, card2, card3};
 }
