0e537a9f6c7a7f187ea72335421eb34d207c7cb2
AzarulM <43213109+AzarulM@users.noreply.github.com>
2019-01-11 11:40:03 -0500

    Update DeckTester.java

--- src/Activity2/DeckTester.java
+++ src/Activity2/DeckTester.java
@@ -11,6 +11,16 @@ public class DeckTester {
      */
     public static void main(String[] args) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+
+        String[] ranks = {"Ace","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Jack","Queen","King"};
+        String[] suits = {"Diamonds","Clubs","Hearts","Spades"};
+        int[] pointValues = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+        Deck deck = new Deck(ranks, suits, pointValues);
+        deck.shuffle();
+        while (!deck.isEmpty()) {
+            System.out.println(deck.deal());
+        }
+
     }
 }
 
